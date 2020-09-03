ef7b410b0606d7e3e7ca5f1fca583d1fc894e638
a-abidi <aliabidi@icloud.com>
2020-09-03 09:00:27 +0100

    Add required functions in Calculator Brain.

--- BMI Calculator/Models/CalculatorBrain.swift
+++ BMI Calculator/Models/CalculatorBrain.swift
@@ -7,3 +7,18 @@
 //
 
 import Foundation
+
+struct CalculatorBrain {
+    
+    var bmi: Float
+    
+    func getBMIValue() -> String {
+        return String(format: "%.1f", bmi)
+    }
+    
+    mutating func calculateBMI(height: Float, weight: Float) {
+        bmi = weight / (height * height)
+    }
+    
+    
+}
